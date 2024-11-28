2ec2f002874c679cf89840c7b2b6883d6941d4a6
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-11-28 16:29:42 +0300

    Completed q3

--- q3.py
+++ q3.py
@@ -1,1 +1,8 @@
 # Write a Python program to print all numbers between two user-provided integers, skipping numbers divisible by 7.
+
+num1 = int(input('Enter number 1:'))
+num2 = int(input('Enter number 2:'))
+
+for num in range(num1, num2):
+    if num % 7 != 0:
+        print(num)
